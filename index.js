712097a365a182e53806537be5d430248feda051
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-03-25 11:13:10 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+const body = document.getElementsByTagName("body")[0];
+const mainn = body.querySelector("main");
+body.removeChild(mainn);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Tova is the champion";
+body.append(newHeader);
